acc628423275a7eae8bf4c9092071dd70ca39369
kjuiop <copying_y@naver.com>
2020-05-16 14:26:02 +0900

    Initial commit

---
+++ src/main/java/com/springboot/study/demo/StartSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.study.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/study/demo/StartSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.study.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
