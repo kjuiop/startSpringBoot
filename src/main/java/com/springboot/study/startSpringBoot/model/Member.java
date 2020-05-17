881f66b594d5efddd0e38f88ed3bd2539261bcfd
kjuiop <copying_y@naver.com>
2020-05-17 22:40:45 +0900

    entity test

--- src/main/java/com/springboot/study/startSpringBoot/model/Member.java
+++ src/main/java/com/springboot/study/startSpringBoot/model/Member.java
@@ -1,4 +1,22 @@
 package com.springboot.study.startSpringBoot.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
 public class Member {
+
+    private Long memberId;
+    private String title;
+    private String writer;
+    private String content;
+
+    private LocalDateTime regDate;
+    private LocalDateTime updateDate;
+
 }
